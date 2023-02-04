dbeca7f0961e14f01c53f895820285d99caeff8f
gato365 <immanuelwilliams@gato365.com>
2023-02-04 13:52:19 -0800

    Develop server

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+
+const sequelize = require("./config/connection");
+const userRouter = require("./controllers/userApis");
+
+
+const PORT = process.env.PORT || 3001;
+
+
+const app = express();
+
+app.use(express.json());
+// This line is needed to parse the body of the request
+app.use(express.urlencoded({ extended: true }));
+app.use("/user", userRouter);
+
+sequelize.sync( ).then(() => {
+    app.listen(PORT, () => 
+    {
+        console.log(`App listening on port ${PORT}!`);
+    }
+    );
+});
